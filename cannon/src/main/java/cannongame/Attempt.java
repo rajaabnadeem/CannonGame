b54738fa592e177f860eb7ed4b977e62a51095e1
Mansour Dione <soura934@gmail.com>
2021-09-30 16:23:59 -0400

    Attempts class made

---
+++ cannon/src/main/java/cannongame/Attempt.java
@@ -0,0 +1,19 @@
+package cannongame;
+
+public class Attempt implements IAttempt{
+
+    private int shot;
+    public Attempt() {
+        shot = 0;
+    }
+
+
+    public int increaseShotAttempt(){
+        shot++;
+        return shot;
+    }
+
+    public int getShot() {
+        return shot;
+    }
+}
