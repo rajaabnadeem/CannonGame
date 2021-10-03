418ac613b83c58b32c20db3d604d5a651c9b3623
Mansour Dione <soura934@gmail.com>
2021-10-03 14:04:34 -0400

    Create the Shot Flow Test and Shot Flow Class

---
+++ cannon/src/main/java/cannongame/ShotFlow.java
@@ -0,0 +1,20 @@
+package cannongame;
+
+public class ShotFlow implements IShotFlow{
+
+    public boolean shoot(int targetX, int targetY, IAngleFlow angleFlow,
+                         IVelocityFlow velocityFlow, IDegreeConversion degreeConversion,
+                         IGettingFinalXCoordinate finalXCoordinate, ICalculateYValue finalYCoordinate,
+                         IRoundingObject roundingObject, ITargetHit targetHit
+    ) {
+        int angle = angleFlow.getAngle();
+        int velocity = velocityFlow.getVelocity();
+        double degree = degreeConversion.ConvertToDegrees(angle);
+        double X = finalXCoordinate.gettingFinalXCoordinate(degree, velocity);
+        double Y = finalYCoordinate.YValue(degree, velocity);
+        int finalX = roundingObject.roundNumber(X);
+        int finalY = roundingObject.roundNumber(Y);
+        System.out.println("Your shot hit X: " + finalX + " Y: " + finalY);
+        return targetHit.hit(finalX, finalY, targetX, targetY);
+    }
+}
--- cannon/src/test/java/cannongame/AngleTest.java
+++ cannon/src/test/java/cannongame/AngleTest.java
@@ -2,7 +2,7 @@ package cannongame;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.*;   
 
 
 
